63bd705f134214d8b25c006c281da400f308d529
Hector <hectorbennett@outlook.com>
2023-05-10 15:40:55 +0100

    send messages in both directions

--- src/ui/src/App.tsx
+++ src/ui/src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { useEffect, useState } from "react";
 import { css } from "@emotion/react";
 import "./App.css";
 import Pedal from "./Pedal";
 
 const sendToPlugin = (msg: unknown) => {
-  /* This is picked up by nih_plug_webview */
+  /* Send message to nih_plug_webview */
   try {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     window.sendToPlugin(msg);
   } catch {
@@ -22,10 +22,22 @@ function App() {
   });
 
   const setGain = (gain: number) => {
-    console.log(gain);
     setPedalState((s) => ({ ...s, gain }));
     sendToPlugin({ type: "SetGain", value: gain });
   };
+
+  useEffect(() => {
+    /* receive messages from nih_plug_webview */
+    // @ts-ignore
+    window.onPluginMessage = (msg) => {
+      switch (msg.type) {
+        case "param_change": {
+          setPedalState((s) => ({ ...s, [msg.param]: msg.value }));
+          break;
+        }
+      }
+    };
+  }, []);
 
   return (
     <div

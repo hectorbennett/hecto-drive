9478667994dc45be03efb13ecc90f58185cd3de5
Hector <hectorbennett@outlook.com>
2023-12-27 01:43:41 +0000

    tests

---
+++ src/ui/jest.config.ts
@@ -0,0 +1,13 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  // A preset that is used as a base for Jest's configuration
+  preset: "ts-jest",
+};
+
+export default config;
---
+++ src/ui/src/utils.test.ts
@@ -0,0 +1,18 @@
+import { fractionToValue, round, valueToFraction } from "./utils";
+
+test("Can convert a fraction to a value", () => {
+  expect(fractionToValue(0.5, 100, 200)).toEqual(150);
+  expect(fractionToValue(0.5, -100, 100)).toEqual(0);
+  expect(fractionToValue(0, 0, 100)).toEqual(0);
+})
+
+test("Can convert a value to a fraction", () => {
+  expect(valueToFraction(150, 100, 200)).toEqual(0.5);
+  expect(valueToFraction(0, -100, 100)).toEqual(0.5);
+  expect(valueToFraction(0, 0, 100)).toEqual(0);
+})
+
+test("Can round numbers", () => {
+  expect(round(1.0, 2)).toEqual("1");
+  expect(round(1.123, 2)).toEqual("1.12");
+})

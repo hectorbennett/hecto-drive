d4eedfa16484424f0d63fa7575a51f85bf1438fe
Hector <hectorbennett@outlook.com>
2023-12-27 01:50:04 +0000

    format fix

--- src/ui/jest.config.ts
+++ src/ui/jest.config.ts
@@ -1,8 +1,3 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 import type { Config } from "jest";
 
 const config: Config = {
